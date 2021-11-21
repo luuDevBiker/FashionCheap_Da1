5334b04d3f350922ca283d0ae8cf6cfb071efe1b
luuDevBiker <luubiker@gmail.com>
2021-11-21 20:27:20 +0700

    Luu commit

---
+++ _2_BUS/Models/Order_OrderDetail.cs
@@ -0,0 +1,15 @@
+﻿using DAL_DataAccessLayers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_BUS.Models
+{
+    public class Order_OrderDetail
+    {
+        ORDERS ORDERS { get; set; }
+
+    }
+}
