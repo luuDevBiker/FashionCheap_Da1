efa0366dc0914d32d2f1f645f39d7adb73fcb6b1
PhiThiTrangg <trangptph15762@fpt.edu.vn>
2021-12-01 22:33:35 +0700

    comit

--- _3_GUI/FrmSanPham.cs
+++ _3_GUI/FrmSanPham.cs
@@ -45,24 +45,24 @@ namespace _3_GUI
 
         void loadata()
         {
-            data_Sanpham.ColumnCount = 15 + PS_BUS.getCountOption().Count();
+            data_ThongTinSanPham1.ColumnCount = 15 + PS_BUS.getCountOption().Count();
             var row = 0;
-            data_Sanpham.Columns[row++].Name = "Tên Sản Phẩm";
-            data_Sanpham.Columns[row++].Name = "Mã sản phẩm";
-            data_Sanpham.Columns[row++].Name = "Số Lượng";
-            data_Sanpham.Columns[row++].Name = "Gía Nhập";
-            data_Sanpham.Columns[row++].Name = "Gía Bán";
-            data_Sanpham.Columns[row].Name = "Id Sản Phẩm";
-            data_Sanpham.Columns[row++].Visible = false;
-            data_Sanpham.Columns[row].Name = "Id variant ";
-            data_Sanpham.Columns[row++].Visible = false;
+            data_ThongTinSanPham1.Columns[row++].Name = "Tên Sản Phẩm";
+            data_ThongTinSanPham1.Columns[row++].Name = "Mã sản phẩm";
+            data_ThongTinSanPham1.Columns[row++].Name = "Số Lượng";
+            data_ThongTinSanPham1.Columns[row++].Name = "Gía Nhập";
+            data_ThongTinSanPham1.Columns[row++].Name = "Gía Bán";
+            data_ThongTinSanPham1.Columns[row].Name = "Id Sản Phẩm";
+            data_ThongTinSanPham1.Columns[row++].Visible = false;
+            data_ThongTinSanPham1.Columns[row].Name = "Id variant ";
+            data_ThongTinSanPham1.Columns[row++].Visible = false;
             PS_BUS.getCountOption().ForEach(x =>
             {
-                data_Sanpham.Columns[row].Name = "Id option " + x.id_Option;
-                data_Sanpham.Columns[row++].Visible = false;
-                data_Sanpham.Columns[row].Name = "Id Value" + x.id_Option;
-                data_Sanpham.Columns[row++].Visible = false;
-                data_Sanpham.Columns[row++].Name = x.option_Name;
+                data_ThongTinSanPham1.Columns[row].Name = "Id option " + x.id_Option;
+                data_ThongTinSanPham1.Columns[row++].Visible = false;
+                data_ThongTinSanPham1.Columns[row].Name = "Id Value" + x.id_Option;
+                data_ThongTinSanPham1.Columns[row++].Visible = false;
+                data_ThongTinSanPham1.Columns[row++].Name = x.option_Name;
             });
             DataGridViewComboBoxColumn comboBox = new DataGridViewComboBoxColumn();
             {
@@ -70,7 +70,7 @@ namespace _3_GUI
                 comboBox.HeaderText = "Chức năng";
                 comboBox.Items.Add("Update");
                 comboBox.Items.Add("Delete");
-                data_Sanpham.Columns.Add(comboBox);
+                data_ThongTinSanPham1.Columns.Add(comboBox);
             }
             DataGridViewButtonColumn button = new DataGridViewButtonColumn();
             {
@@ -78,9 +78,9 @@ namespace _3_GUI
                 button.HeaderText = "OK";
                 button.Text = ("OK");
                 button.UseColumnTextForButtonValue = true;
-                this.data_Sanpham.Columns.Add(button);
+                this.data_ThongTinSanPham1.Columns.Add(button);
             }
-            data_Sanpham.Rows.Clear();
+            data_ThongTinSanPham1.Rows.Clear();
 
             foreach (var x in _litSanPhamCuThes)
             {
@@ -98,85 +98,10 @@ namespace _3_GUI
                     objcell.Add(i.id_Values + "");
                     objcell.Add(i.option_Values + "");
                 });
-                data_Sanpham.Rows.Add(objcell.ToArray());
+                data_ThongTinSanPham1.Rows.Add(objcell.ToArray());
 
             }
         }
-        private void data_Sanpham_CellClick(object sender, DataGridViewCellEventArgs e)
-        {
-            var index = e.RowIndex;
-            var row = data_Sanpham.Rows[index];
-            txtTenSp.Text = row.Cells[0].Value + "";
-            txtMaSP.Text = row.Cells[1].Value + "";
-            txtSL.Text = row.Cells[2].Value + "";
-            txtNhap.Text = row.Cells[3].Value + "";
-            txtBan.Text = row.Cells[4].Value + "";
-            //txtMau.Text = row.Cells[9].Value + "";
-            //txtSize.Text = row.Cells[12].Value + "";
-            //txtTheLoai.Text = row.Cells[15].Value + "";
-            //txtThuonghieu.Text = row.Cells[18].Value + "";
-        }
-        private void tbxLocThuongHieu_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void tbxLocMau_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void tbxLocKichThuoc_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void data_Sanpham_CellContentClick(object sender, DataGridViewCellEventArgs e)
-        {
-            int index = e.RowIndex;
-
-        }
-
-        private void btnThem_Click(object sender, EventArgs e)
-        {
-            PRODUCTS products = new PRODUCTS();
-
-        }
-
-        private void btnSua_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void btnXoa_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void btnPhanloai_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void FrmSanPham_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        private void lblTheLoai_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void btnAddNewPrd_Click(object sender, EventArgs e)
-        {
-            if (txtTenSp.Text.Length == 0)
-            {
-                MessageBox.Show("Chưa nhập tên sản phẩm", "ERROR");
-                return;
-            }
-            var name = txtTenSp.Text;
-            MessageBox.Show(PS_BUS.addNewProduct(name));
-        }
+      
     }
 }
