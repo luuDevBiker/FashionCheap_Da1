a4262e7dd3de7a0afc3858e0746231dbb5faeaef
trang thuy <trangthuy.19102002@gmail.com>
2021-12-05 20:02:23 +0700

    thông kê đã XOng. Anh Lưu Làm Lốt nhá

---
+++ _2_BUS/Models/DTTam.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_BUS.Models
+{
+    public class DTTam
+    {
+        private int a;
+        private int b;
+
+        public DTTam()
+        {
+            
+        }
+
+        public DTTam(int a, int b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+        public int A
+        {
+            get => a;
+            set => a = value;
+        }
+
+        public int B
+        {
+            get => b;
+            set => b = value;
+        }
+    }
+}
--- _3_GUI/FrmThongKeSanPham.Designer.cs
+++ _3_GUI/FrmThongKeSanPham.Designer.cs
@@ -32,38 +32,30 @@ namespace _3_GUI
             this.radioButton1 = new System.Windows.Forms.RadioButton();
             this.radioButton2 = new System.Windows.Forms.RadioButton();
             this.radioButton3 = new System.Windows.Forms.RadioButton();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.MaSp = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.TenSP = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.LoaiSP = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.NgayNhap = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.SLNhap = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.DonGia = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.ConLai = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.LoiNhuan = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.MoTa = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.SoLuong = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.HinhAnh = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.DGV_TKSanPham = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.DGV_TKSanPham)).BeginInit();
             this.SuspendLayout();
             // 
             // radioButton1
             // 
             this.radioButton1.AutoSize = true;
-            this.radioButton1.Location = new System.Drawing.Point(101, 34);
+            this.radioButton1.Location = new System.Drawing.Point(81, 27);
+            this.radioButton1.Margin = new System.Windows.Forms.Padding(2);
             this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(86, 29);
+            this.radioButton1.Size = new System.Drawing.Size(72, 24);
             this.radioButton1.TabIndex = 0;
             this.radioButton1.TabStop = true;
             this.radioButton1.Text = "Tất Cả";
             this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
             // 
             // radioButton2
             // 
             this.radioButton2.AutoSize = true;
-            this.radioButton2.Location = new System.Drawing.Point(408, 34);
+            this.radioButton2.Location = new System.Drawing.Point(326, 27);
+            this.radioButton2.Margin = new System.Windows.Forms.Padding(2);
             this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(192, 29);
+            this.radioButton2.Size = new System.Drawing.Size(158, 24);
             this.radioButton2.TabIndex = 1;
             this.radioButton2.TabStop = true;
             this.radioButton2.Text = "Sản Phẩm bán chạy";
@@ -72,126 +64,42 @@ namespace _3_GUI
             // radioButton3
             // 
             this.radioButton3.AutoSize = true;
-            this.radioButton3.Location = new System.Drawing.Point(700, 34);
+            this.radioButton3.Location = new System.Drawing.Point(560, 27);
+            this.radioButton3.Margin = new System.Windows.Forms.Padding(2);
             this.radioButton3.Name = "radioButton3";
-            this.radioButton3.Size = new System.Drawing.Size(253, 29);
+            this.radioButton3.Size = new System.Drawing.Size(207, 24);
             this.radioButton3.TabIndex = 2;
             this.radioButton3.TabStop = true;
             this.radioButton3.Text = "Sản Phẩm không bán được";
             this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
             // 
-            // dataGridView1
+            // DGV_TKSanPham
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.MaSp,
-            this.TenSP,
-            this.LoaiSP,
-            this.NgayNhap,
-            this.SLNhap,
-            this.DonGia,
-            this.ConLai,
-            this.LoiNhuan,
-            this.MoTa,
-            this.SoLuong,
-            this.HinhAnh});
-            this.dataGridView1.Location = new System.Drawing.Point(12, 82);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowHeadersWidth = 62;
-            this.dataGridView1.RowTemplate.Height = 33;
-            this.dataGridView1.Size = new System.Drawing.Size(1718, 640);
-            this.dataGridView1.TabIndex = 3;
-            // 
-            // MaSp
-            // 
-            this.MaSp.HeaderText = "MaSP";
-            this.MaSp.MinimumWidth = 8;
-            this.MaSp.Name = "MaSp";
-            this.MaSp.Width = 150;
-            // 
-            // TenSP
-            // 
-            this.TenSP.HeaderText = "TenSP";
-            this.TenSP.MinimumWidth = 8;
-            this.TenSP.Name = "TenSP";
-            this.TenSP.Width = 150;
-            // 
-            // LoaiSP
-            // 
-            this.LoaiSP.HeaderText = "LoaiSP";
-            this.LoaiSP.MinimumWidth = 8;
-            this.LoaiSP.Name = "LoaiSP";
-            this.LoaiSP.Width = 150;
-            // 
-            // NgayNhap
-            // 
-            this.NgayNhap.HeaderText = "NgayNhap";
-            this.NgayNhap.MinimumWidth = 8;
-            this.NgayNhap.Name = "NgayNhap";
-            this.NgayNhap.Width = 150;
-            // 
-            // SLNhap
-            // 
-            this.SLNhap.HeaderText = "SLNhap";
-            this.SLNhap.MinimumWidth = 8;
-            this.SLNhap.Name = "SLNhap";
-            this.SLNhap.Width = 150;
-            // 
-            // DonGia
-            // 
-            this.DonGia.HeaderText = "DonGia";
-            this.DonGia.MinimumWidth = 8;
-            this.DonGia.Name = "DonGia";
-            this.DonGia.Width = 150;
-            // 
-            // ConLai
-            // 
-            this.ConLai.HeaderText = "Còn Lại";
-            this.ConLai.MinimumWidth = 8;
-            this.ConLai.Name = "ConLai";
-            this.ConLai.Width = 150;
-            // 
-            // LoiNhuan
-            // 
-            this.LoiNhuan.HeaderText = "Lợi Nhuận";
-            this.LoiNhuan.MinimumWidth = 8;
-            this.LoiNhuan.Name = "LoiNhuan";
-            this.LoiNhuan.Width = 150;
-            // 
-            // MoTa
-            // 
-            this.MoTa.HeaderText = "Mô Tả";
-            this.MoTa.MinimumWidth = 8;
-            this.MoTa.Name = "MoTa";
-            this.MoTa.Width = 150;
-            // 
-            // SoLuong
-            // 
-            this.SoLuong.HeaderText = "Số Lượng";
-            this.SoLuong.MinimumWidth = 8;
-            this.SoLuong.Name = "SoLuong";
-            this.SoLuong.Width = 150;
-            // 
-            // HinhAnh
-            // 
-            this.HinhAnh.HeaderText = "Hình Ảnh";
-            this.HinhAnh.MinimumWidth = 8;
-            this.HinhAnh.Name = "HinhAnh";
-            this.HinhAnh.Width = 150;
+            this.DGV_TKSanPham.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DGV_TKSanPham.Location = new System.Drawing.Point(10, 66);
+            this.DGV_TKSanPham.Margin = new System.Windows.Forms.Padding(2);
+            this.DGV_TKSanPham.Name = "DGV_TKSanPham";
+            this.DGV_TKSanPham.RowHeadersWidth = 62;
+            this.DGV_TKSanPham.RowTemplate.Height = 33;
+            this.DGV_TKSanPham.Size = new System.Drawing.Size(1374, 512);
+            this.DGV_TKSanPham.TabIndex = 3;
             // 
             // FrmThongKeSanPham
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(239)))), ((int)(((byte)(156)))), ((int)(((byte)(164)))));
-            this.ClientSize = new System.Drawing.Size(1742, 746);
-            this.Controls.Add(this.dataGridView1);
+            this.ClientSize = new System.Drawing.Size(1394, 597);
+            this.Controls.Add(this.DGV_TKSanPham);
             this.Controls.Add(this.radioButton3);
             this.Controls.Add(this.radioButton2);
             this.Controls.Add(this.radioButton1);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "FrmThongKeSanPham";
             this.Text = "FrmThongKeSanPham";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.Load += new System.EventHandler(this.FrmThongKeSanPham_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.DGV_TKSanPham)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -202,17 +110,6 @@ namespace _3_GUI
         private System.Windows.Forms.RadioButton radioButton1;
         private System.Windows.Forms.RadioButton radioButton2;
         private System.Windows.Forms.RadioButton radioButton3;
-        private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.DataGridViewTextBoxColumn MaSp;
-        private System.Windows.Forms.DataGridViewTextBoxColumn TenSP;
-        private System.Windows.Forms.DataGridViewTextBoxColumn LoaiSP;
-        private System.Windows.Forms.DataGridViewTextBoxColumn NgayNhap;
-        private System.Windows.Forms.DataGridViewTextBoxColumn SLNhap;
-        private System.Windows.Forms.DataGridViewTextBoxColumn DonGia;
-        private System.Windows.Forms.DataGridViewTextBoxColumn ConLai;
-        private System.Windows.Forms.DataGridViewTextBoxColumn LoiNhuan;
-        private System.Windows.Forms.DataGridViewTextBoxColumn MoTa;
-        private System.Windows.Forms.DataGridViewTextBoxColumn SoLuong;
-        private System.Windows.Forms.DataGridViewTextBoxColumn HinhAnh;
+        private System.Windows.Forms.DataGridView DGV_TKSanPham;
     }
 }
